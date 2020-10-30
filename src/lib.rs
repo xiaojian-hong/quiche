bd316b53926d23478b9c956591198e57622a4901
Junho Choi <junho@cloudflare.com>
2020-10-29 18:24:27 -0700

    lib: limiting a burst of outgoing packets
    "QUIC draft-32 7.7 Pacing" says:
       Sending multiple packets into the network without any delay between
       them creates a packet burst that might cause short-term congestion
       and losses.  Senders MUST either use pacing or limit such bursts.
       Senders SHOULD limit bursts to the initial congestion window; see
       Section 7.2.  A sender with knowledge that the network path to the
       receiver can absorb larger bursts MAY use a higher limit.
    
    When I test quiche in a high bandwidth/low rtt environment such as localhost,
    I observe outgoing packet burst creates a lot of loss and messing up
    recovery control, leading timeout or other errors.
    
    Since we don't do packet pacing yet, this PR implements limiting outgoing
    packet burst. As recommeneded in the draft, now we limit total number of
    outgoing packets in a single send() loop to the initial congestion window.
    
    However, this may be too conservative when the interface can handle
    more packet burst. When I only limit burst to initcwnd, it's slower than
    unlimited burst (when there is no error). So, this limit can be tuned to
    min(cwnd/2, initcwnd) until we see the packet loss.
    With this heuristic, we can find a balance between the high performance
    and less packet loss due to the burst.
    
    This works at quiche library level, so no application change is required.

--- src/lib.rs
+++ src/lib.rs
@@ -809,6 +809,12 @@ pub struct Connection {
     /// Total number of sent packets.
     sent_count: usize,
 
+    /// Total number of sent bytes without calling recv().
+    sent_bytes_burst: usize,
+
+    /// Max number of sent bytes without calling recv().
+    sent_bytes_max_burst: usize,
+
     /// Total number of bytes received from the peer.
     rx_data: u64,
 
@@ -1158,6 +1164,9 @@ impl Connection {
 
             recv_count: 0,
             sent_count: 0,
+
+            sent_bytes_burst: 0,
+            sent_bytes_max_burst: recovery::INITIAL_WINDOW,
 
             rx_data: 0,
             max_rx_data,
@@ -1934,6 +1943,20 @@ impl Connection {
     /// # Ok::<(), quiche::Error>(())
     /// ```
     pub fn send(&mut self, out: &mut [u8]) -> Result<usize> {
+        // Burst protection: don't send more than `sent_bytes_max_burst`
+        // bytes when calling send() consecutively. This counter will be
+        // reset when calling recv().
+        // Note that this need to be disabled if pacing enabled
+        if self.sent_bytes_burst > self.sent_bytes_max_burst {
+            if self.recovery.lost_count == 0 {
+                // Try to increase the burst limit if there is no loss.
+                self.sent_bytes_max_burst =
+                    cmp::max(recovery::INITIAL_WINDOW, self.recovery.cwnd() / 2);
+            }
+            self.sent_bytes_burst = 0;
+            return Err(Error::Done);
+        }
+
         let now = time::Instant::now();
 
         if out.is_empty() {
@@ -2594,6 +2617,8 @@ impl Connection {
         if ack_eliciting {
             self.ack_eliciting_sent = true;
         }
+
+        self.sent_bytes_burst += written;
 
         Ok(written)
     }
